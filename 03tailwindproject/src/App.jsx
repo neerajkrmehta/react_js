722f0b783ec61fc62113dea46bf4400fc794c749
Neeraj Kumar Mehta <neerajkumarmehta325@gmail.com>
2023-09-20 10:02:45 +0530

    some update

--- 03tailwindproject/src/App.jsx
+++ 03tailwindproject/src/App.jsx
@@ -6,12 +6,7 @@ import Card from './component/Card'
 
 
 function App() {
-  const [count, setCount] = useState(0)
-  let myObj = {
-    username:"neerajrz",
-    age:23
-  }
-  let newArr = [1,2,3]
+  // const [count, setCount] = useState(0)
   return (
     <>
      <h1 className='bg-green-400 text-black p-4 rounded-xl mb-5'>TailWind test</h1>
